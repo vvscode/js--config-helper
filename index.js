bdfced87d447bae190991a7f4992eae3b5fe202a
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-16 09:44:09 +0400

    Skip example file from processing
    Small improvements for config-template
    
    Update helper for get requests

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const util = require('util');
 const fsExtra = require('fs-extra');
 const esbTemplate = require('./utils/esb-template');
 
-const pRequest = (options) => new Promise((resolve, reject) => request(options, (err, response, body) => err ? reject(err) : resolve([response, body])));
+const safeParse = str => {
+  try {
+    return JSON.parse(`${str}`);
+  } catch(e) {
+    return str;
+  }
+}
+const pRequest = (options) => new Promise((resolve, reject) => request(options, (err, response, body) => err ? resolve(console.log(err), {
+  note: `Request does not work at the moment: ${err}`
+}) : resolve([response, safeParse(body)])));
 
 
 const files = glob.sync('input/**/*.json5');
 
-(async () => files.forEach(async(fileName) => {
+(async () => files.filter(fileName => !fileName.match('input/example/')).forEach(async(fileName) => {
     console.log(`[=] Processing "${fileName}"`);
     try {
       const requestOptions = require(`./${fileName}`);
--- utils/esb-template.js
+++ utils/esb-template.js
@@ -1,5 +1,18 @@
 const _ = require('lodash');
 
+const limitListSizes = (obj, maxLen = 2) => {
+  Object.keys(obj).forEach((key) => {
+    const item = obj[key];
+    if (Array.isArray(item)) {
+      item.length = Math.min(item.length, maxLen);
+    }
+    if (item && typeof item === 'object') {
+      limitListSizes(item, maxLen);
+    }
+  });
+  return obj;
+}
+
 const objectWithKeySorted = object =>
   Object.keys(object)
     .sort()
@@ -9,15 +22,18 @@ const objectWithKeySorted = object =>
     }, {});
 
 const getRequestKeys = obj =>
-  objectWithKeySorted(
+  obj ? objectWithKeySorted(
     Object.keys(obj).reduce((acc, key) => {
+      if (`${key}`.match(/^\d+$/)) {
+        return acc;
+      }
       acc[key] = _.camelCase(key);
       if (_.isObject(obj[key])) {
         Object.assign(acc, getRequestKeys(obj[key]));
       }
       return acc;
     }, {}),
-  );
+  ) : obj;
 
 const removeDummyWrappers = (obj, list = []) => {
   if (!_.isObject(obj)) {
@@ -32,6 +48,10 @@ const removeDummyWrappers = (obj, list = []) => {
 };
 
 module.exports = (requestOptions, responseBody) => {
+  responseBody = limitListSizes(responseBody);
+  console.log('responseBody', responseBody);
+
+  const method = (requestOptions.method || 'post').toLowerCase();
   const url = requestOptions.url.replace(/https?:\/\/[^/]+/, '${config.host}');
   const name = requestOptions.url.replace(/https?:\/\/[^/]+/, '');
   const esbRequestExample = JSON.stringify(requestOptions.body, null, 2);
@@ -65,14 +85,14 @@ module.exports = (requestOptions, responseBody) => {
   const responseUnwrapper =
     responseWrappers.length === 0
       ? 'obj => obj'
-      : `obj => obj.${responseWrappers.join('.')}`;
+      : `(obj: any) => obj.${responseWrappers.join('.')}`;
 
   return `
   export default (config: IEsbDeclarationConfig): IEsbDefinition => ({
     name: '${name}',
-    method: 'post',
-    url: '${url}',
-    defaultKey: config.API_KEY,
+    method: '${method}',
+    url: \`${url}\`,
+    defaultKey: config.REPLACE_WITH_CORRECT_NAME_OF_API_KEY,
     description: 'Looking for information =(',
     esbRequestExample: ${esbRequestExample},
     esbResponseExample: ${esbResponseExample},
